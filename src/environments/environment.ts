b7893f98172ab9d00859dedc23e64f6058a7a39f
Juan Ignacio Carrozzo <juanignacio@sisorg.com>
2022-07-11 11:59:17 +0300

    cleaning

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyD5w2Mw63EuBU0AkcOobcqjf_YJQCuqZ7k',
-    authDomain: 'baby-log-914ac.firebaseapp.com',
-    projectId: 'baby-log-914ac',
-    storageBucket: 'baby-log-914ac.appspot.com',
-    messagingSenderId: '271395923209',
-    appId: '1:271395923209:web:0551e9fe7eba9755631fd8',
-    measurementId: 'G-2VE85MR0Y3'
+    // Replace with your data
   }
 };
 
